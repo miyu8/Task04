ade8948dd233317340cfb27fc2a2ede56530d476
Michael Lobanov <miyu8@mail.ru>
2016-03-15 01:30:34 +0300

    Task04 version 2

---
+++ NUnitTestProject/CalculatorTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Calculator
+{
+    [TestFixture]
+    class CalculatorTest
+    {
+        [Test]
+        public static void TestOperations()
+        {
+            Calculator Calc = new Calculator();
+            Assert.AreEqual(44, Calc.Add("12,32"));
+        }
+
+        [Test]
+        public static void TestOperations2()
+        {
+            Calculator Calc = new Calculator();
+            Assert.AreEqual(94, Calc.Add2("12,3,1,78"));
+        }
+
+        [Test]
+        public static void TestOperations3()
+        {
+            Calculator Calc = new Calculator();
+            Assert.AreEqual(94, Calc.Add3("12.3-1)78"));
+        }
+
+        [Test]
+        public static void TestOperations4()
+        {
+            Calculator Calc = new Calculator();
+            Assert.AreNotEqual(94, Calc.Add4("12.3-1)78"));
+            Assert.AreEqual(94, Calc.Add4("12.3)1)78"));
+        }
+
+        [Test]
+        public static void TestOperations5()
+        {
+            Calculator Calc = new Calculator();
+            Assert.AreNotEqual(94, Calc.Add5("12...3*1)-78"));
+            Assert.AreNotEqual(94, Calc.Add5("12...3*-1)78"));
+            Assert.AreEqual(94, Calc.Add5("12...3-*1-)78"));
+            Assert.AreEqual(94, Calc.Add5("12...3*1)78"));
+        }
+
+    }
+}
