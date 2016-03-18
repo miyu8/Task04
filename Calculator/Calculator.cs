375956b59058f145dab18c861a27bd3c1055f9e4
Michael Lobanov <miyu8@mail.ru>
2016-03-18 03:03:00 +0300

    Task04 version 3

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -7,133 +7,197 @@ using System.Text.RegularExpressions;
 
 namespace Calculator
 {
+    [Serializable()]
+    public class Exception2 : Exception
+    {
+        public Exception2() : base() { }
+        public Exception2(string message) : base(message) { }
+        public Exception2(string message, System.Exception inner) : base(message, inner) { }
+        protected Exception2(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+        { }
+    }
     public class Calculator
     {
-        public int? Add(string numbers)
-
+        public double Add(string numbers)
         {
-            Regex regex = new Regex(@"^\d+[,]?\d+$");
+            Regex regex = new Regex(@"^[+-]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))[,]{1}[+-]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))$");
             if (regex.IsMatch(numbers))
             {
-                MatchCollection match = Regex.Matches(numbers, @"\d+");
-                Console.WriteLine("Исходная строка:\n\n{0}\n\nСумма:\n", numbers);
-                int summa, chislo;
+                double summa, chislo;
                 summa = chislo = 0;
-                foreach (Match MassivNumbers in match)
+                foreach (string s in Regex.Split(numbers, ","))
                 {
-                    int.TryParse(MassivNumbers.Value, out chislo);
+                    double.TryParse(s.Replace('.', ','), out chislo);
                     summa += chislo;
-
                 }
-                Console.WriteLine(summa);
                 return summa;
             }
-            else
-            {
-                Console.WriteLine("Введите два числа через запятую!");
-                return null;
-            }
+            else        
+                throw new Exception2();
         }
-
-        public int? Add2(string numbers)
-
+        public double Add()
         {
-            Regex regex = new Regex(@"^(\d+[,]?)+\d+$");
-            if (regex.IsMatch(numbers))
+            Regex regex = new Regex(@"^[+-]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))[,]{1}[+-]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))$");
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            if (regex.IsMatch(str))
             {
-                MatchCollection match = Regex.Matches(numbers, @"\d+");
-                Console.WriteLine("Исходная строка:\n\n{0}\n\nСумма:\n", numbers);
-                int summa, chislo;
+                double summa, chislo;
                 summa = chislo = 0;
-                foreach (Match MassivNumbers in match)
+                foreach (string s in Regex.Split(str, ","))
                 {
-                    int.TryParse(MassivNumbers.Value, out chislo);
+                    double.TryParse(s.Replace('.', ','), out chislo);
                     summa += chislo;
-
                 }
-                Console.WriteLine(summa);
                 return summa;
             }
             else
-            {
-                Console.WriteLine("Введите не менее двух чисел через запятую!");
-                return null;
-            }
+                throw new Exception2();
         }
-        public int? Add3(string numbers)
-
+        public double Add2(string numbers)
         {
-            Regex regex = new Regex(@"^(\d+\D?)+\d+$");
+            Regex regex = new Regex(@"^([+-]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))[,]{1})+[+-]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))$");
             if (regex.IsMatch(numbers))
             {
-                MatchCollection match = Regex.Matches(numbers, @"\d+");
-                Console.WriteLine("Исходная строка:\n\n{0}\n\nСумма:\n", numbers);
-                int summa, chislo;
+                double summa, chislo;
                 summa = chislo = 0;
-                foreach (Match MassivNumbers in match)
+                foreach (string s in Regex.Split(numbers, ","))
                 {
-                    int.TryParse(MassivNumbers.Value, out chislo);
+                    double.TryParse(s.Replace('.', ','), out chislo);
                     summa += chislo;
-
                 }
-                Console.WriteLine(summa);
                 return summa;
             }
             else
-            {
-                Console.WriteLine("Введите не менее двух чисел!");
-                return null;
-            }
+                throw new Exception2();
         }
-
-        public int? Add4(string numbers)
+        public double Add2()
         {
-            Regex regex = new Regex(@"^(\d+[^\d\-]?)+\d+$");
-            if (regex.IsMatch(numbers))
+            Regex regex = new Regex(@"^([+-]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))[,]{1})+[+-]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))$");
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            if (regex.IsMatch(str))
             {
-                MatchCollection match = Regex.Matches(numbers, @"\d+");
-                Console.WriteLine("Исходная строка:\n\n{0}\n\nСумма:\n", numbers);
-                int summa, chislo;
+                double summa, chislo;
                 summa = chislo = 0;
-                foreach (Match MassivNumbers in match)
+                foreach (string s in Regex.Split(str, ","))
                 {
-                    int.TryParse(MassivNumbers.Value, out chislo);
+                    double.TryParse(s.Replace('.', ','), out chislo);
                     summa += chislo;
-
                 }
-                Console.WriteLine(summa);
                 return summa;
             }
             else
-            {
-                Console.WriteLine("Введите не менее двух чисел без минусов!");
-                return null;
-            }
+                throw new Exception2();
         }
-
-        public int? Add5(string numbers)
+        public double Add3(string numbers)
         {
-            Regex regex = new Regex(@"^(\d+\D*(?<![\-]))+\d+$");
+            Regex regex = new Regex(@"^([+-]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))[\,\#\@\$\%\! ]{1})+[+-]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))$");
             if (regex.IsMatch(numbers))
             {
-                MatchCollection match = Regex.Matches(numbers, @"\d+");
-                Console.WriteLine("Исходная строка:\n\n{0}\n\nСумма:\n", numbers);
-                int summa, chislo;
+                double summa, chislo;
                 summa = chislo = 0;
-                foreach (Match MassivNumbers in match)
+                foreach (string s in Regex.Split(numbers, @"[\,\#\@\$\%\! ]"))
                 {
-                    int.TryParse(MassivNumbers.Value, out chislo);
+                    double.TryParse(s.Replace('.', ','), out chislo);
                     summa += chislo;
-
                 }
-                Console.WriteLine(summa);
                 return summa;
             }
             else
+                throw new Exception2();
+        }
+        public double Add3()
+        {
+            Regex regex = new Regex(@"^([+-]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))[\,\#\@\$\%\! ]{1})+[+-]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))$");
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            if (regex.IsMatch(str))
             {
-                Console.WriteLine("Введите не менее двух чисел без минусов с любым числом разделителей между ними!");
-                return null;
+                double summa, chislo;
+                summa = chislo = 0;
+                foreach (string s in Regex.Split(str, @"[\,\#\@\$\%\! ]"))
+                {
+                    double.TryParse(s.Replace('.', ','), out chislo);
+                    summa += chislo;
+                }
+                return summa;
             }
+            else
+                throw new Exception2();
+        }
+        public double Add4(string numbers)
+        {
+            Regex regex = new Regex(@"^([+]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))[\,\#\@\$\%\! ]{1})+[+]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))$");
+            if (regex.IsMatch(numbers))
+            {
+                double summa, chislo;
+                summa = chislo = 0;
+                foreach (string s in Regex.Split(numbers, @"[\,\#\@\$\%\! ]"))
+                {
+                    double.TryParse(s.Replace('.', ','), out chislo);
+                    summa += chislo;
+                }
+                return summa;
+            }
+            else
+                throw new Exception2();
+        }
+        public double Add4()
+        {
+            Regex regex = new Regex(@"^([+]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))[\,\#\@\$\%\! ]{1})+[+]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))$");
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            if (regex.IsMatch(str))
+            {
+                double summa, chislo;
+                summa = chislo = 0;
+                foreach (string s in Regex.Split(str, @"[\,\#\@\$\%\! ]"))
+                {
+                    double.TryParse(s.Replace('.', ','), out chislo);
+                    summa += chislo;
+                }
+                return summa;
+            }
+            else
+                throw new Exception2();
+        }
+        public double Add5(string numbers)
+        {
+            Regex regex = new Regex(@"^([+]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))[\,\#\@\$\%\! ]+)+[+]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))$");
+            if (regex.IsMatch(numbers))
+            {
+                double summa, chislo;
+                summa = chislo = 0;
+                foreach (string s in Regex.Split(numbers, @"[\,\#\@\$\%\! ]+"))
+                {
+                    double.TryParse(s.Replace('.', ','), out chislo);
+                    summa += chislo;
+                }
+                return summa;
+            }
+            else
+                throw new Exception2();
+        }
+        public double Add5()
+        {
+            Regex regex = new Regex(@"^([+]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))[\,\#\@\$\%\! ]+)+[+]?((\d+)|(\d+\.?\d*)|(\d\.?\d*E[+-]?\d+))$");
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            if (regex.IsMatch(str))
+            {
+                double summa, chislo;
+                summa = chislo = 0;
+                foreach (string s in Regex.Split(str, @"[\,\#\@\$\%\! ]+"))
+                {
+                    double.TryParse(s.Replace('.', ','), out chislo);
+                    summa += chislo;
+                }
+                return summa;
+            }
+            else
+                throw new Exception2();
         }
     }
 }
